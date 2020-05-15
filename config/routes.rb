4e7447574bf7b269a2c40ce9d07e0bf74e4287fa
mitsuhiko-y <realreborn1021@gmail.com>
2020-05-15 12:00:42 +0900

    update routes.rb
    -what
    users/sessionsコントローラのdestroyアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions => 'users/sessions'   
+  } 
+  devise_scope :user do
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   root to: 'app#new' 
   resources :app
   resources :users
