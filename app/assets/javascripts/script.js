1e747bb83cf50220bd0ca444fbaa49ab1b5b5aef
mitsuhiko-y <realreborn1021@gmail.com>
2020-05-24 23:34:49 +0900

    update users/show
    ##what
    -script.js作成、編集
    -bootstrap記述削除。アコーディオンをjQueryで実装し直し。

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,29 @@
+$(function(){
+
+  $('.liked_posts_button').click(
+    function() {
+      $('.liked_cards_body').slideDown();
+    }
+  );
+
+  $('.commented_posts_button').click(
+    function() {
+      $('.commented_cards_body').slideDown();
+    }
+  );
+
+});
+
+// 条件分岐
+// $(function(){
+//   $('.commented_posts_button').click(function() {
+//     var $post = $(this).find('.card-body');
+//     if($post.hasClass('open')) {
+//       $post.slideUp();
+//       $post.removeClass('open');
+//     } else {
+//       $post.slideDown();
+//       $post.addClass('open');
+//     }    
+//   });
+// });
